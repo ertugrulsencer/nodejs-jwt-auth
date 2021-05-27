f4103a93d29472d042086867d91ad212c4e79e77
Ertugrul <teknoertugrul@gmail.com>
2021-05-27 17:56:38 +0300

    Router removed

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
+require("dotenv").config();
 const express = require("express");
-const router = require("./router/auth");
 const app = require("express")();
 
 app.use(express.json({ extended: false }));
-
-/* Router Middlewares */
-app.use("/auth", router);
 
 app.listen(5000, () => console.log("Server running"));
